97e22a46d48cde6b34daaf4da76c809c2bb0a0df
Besteekaya <ferfecirokyanus@gmail.com>
2025-03-16 00:08:56 +0300

    Add files via upload

---
+++ ToDoListApp/ToDoListApp/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace ToDoListApp
+{
+    partial class frmToDoList
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTask = new System.Windows.Forms.TextBox();
+            this.lstTasks = new System.Windows.Forms.ListBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblTotalTasks = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnMarkComplete = new System.Windows.Forms.Button();
+            this.chkDarkMode = new System.Windows.Forms.CheckBox();
+            this.btnChangeTheme = new System.Windows.Forms.Button();
+            this.dtpDueDate = new System.Windows.Forms.DateTimePicker();
+            this.btnAddWithDate = new System.Windows.Forms.Button();
+            this.grpPriority = new System.Windows.Forms.GroupBox();
+            this.rdoHigh = new System.Windows.Forms.RadioButton();
+            this.rdoMedium = new System.Windows.Forms.RadioButton();
+            this.rdoLow = new System.Windows.Forms.RadioButton();
+            this.grpPriority.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTask
+            // 
+            this.txtTask.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTask.Location = new System.Drawing.Point(20, 50);
+            this.txtTask.Name = "txtTask";
+            this.txtTask.Size = new System.Drawing.Size(320, 34);
+            this.txtTask.TabIndex = 0;
+            // 
+            // lstTasks
+            // 
+            this.lstTasks.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.lstTasks.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lstTasks.FormattingEnabled = true;
+            this.lstTasks.ItemHeight = 28;
+            this.lstTasks.Location = new System.Drawing.Point(20, 90);
+            this.lstTasks.Name = "lstTasks";
+            this.lstTasks.Size = new System.Drawing.Size(450, 200);
+            this.lstTasks.TabIndex = 1;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI Black", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTitle.ForeColor = System.Drawing.Color.Navy;
+            this.lblTitle.Location = new System.Drawing.Point(125, 10);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(250, 30);
+            this.lblTitle.TabIndex = 2;
+            this.lblTitle.Text = "TO-DO LIST";
+            // 
+            // lblTotalTasks
+            // 
+            this.lblTotalTasks.AutoSize = true;
+            this.lblTotalTasks.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lblTotalTasks.Font = new System.Drawing.Font("Segoe UI Black", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTotalTasks.Location = new System.Drawing.Point(20, 340);
+            this.lblTotalTasks.Name = "lblTotalTasks";
+            this.lblTotalTasks.Size = new System.Drawing.Size(143, 28);
+            this.lblTotalTasks.TabIndex = 3;
+            this.lblTotalTasks.Text = "Total Tasks : ";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.LightGreen;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnAdd.Location = new System.Drawing.Point(350, 50);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(120, 30);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add Task";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.BackColor = System.Drawing.Color.LightBlue;
+            this.btnEdit.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnEdit.Location = new System.Drawing.Point(20, 300);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(140, 30);
+            this.btnEdit.TabIndex = 5;
+            this.btnEdit.Text = "Edit Task";
+            this.btnEdit.UseVisualStyleBackColor = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.LightCoral;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.Location = new System.Drawing.Point(180, 300);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(140, 30);
+            this.btnDelete.TabIndex = 6;
+            this.btnDelete.Text = "Delete Task";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnMarkComplete
+            // 
+            this.btnMarkComplete.BackColor = System.Drawing.Color.Orange;
+            this.btnMarkComplete.Font = new System.Drawing.Font("Segoe UI Black", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnMarkComplete.Location = new System.Drawing.Point(340, 300);
+            this.btnMarkComplete.Name = "btnMarkComplete";
+            this.btnMarkComplete.Size = new System.Drawing.Size(180, 30);
+            this.btnMarkComplete.TabIndex = 7;
+            this.btnMarkComplete.Text = "Mark Completed";
+            this.btnMarkComplete.UseVisualStyleBackColor = false;
+            this.btnMarkComplete.Click += new System.EventHandler(this.btnMarkComplete_Click);
+            // 
+            // chkDarkMode
+            // 
+            this.chkDarkMode.AutoSize = true;
+            this.chkDarkMode.Location = new System.Drawing.Point(20, 30);
+            this.chkDarkMode.Name = "chkDarkMode";
+            this.chkDarkMode.Size = new System.Drawing.Size(96, 20);
+            this.chkDarkMode.TabIndex = 8;
+            this.chkDarkMode.Text = "Dark Mode";
+            this.chkDarkMode.UseVisualStyleBackColor = true;
+            this.chkDarkMode.CheckedChanged += new System.EventHandler(this.chkDarkMode_CheckedChanged);
+            // 
+            // btnChangeTheme
+            // 
+            this.btnChangeTheme.Location = new System.Drawing.Point(154, 378);
+            this.btnChangeTheme.Name = "btnChangeTheme";
+            this.btnChangeTheme.Size = new System.Drawing.Size(128, 23);
+            this.btnChangeTheme.TabIndex = 9;
+            this.btnChangeTheme.Text = "Change Theme";
+            this.btnChangeTheme.UseVisualStyleBackColor = true;
+            this.btnChangeTheme.Click += new System.EventHandler(this.btnChangeTheme_Click);
+            // 
+            // dtpDueDate
+            // 
+            this.dtpDueDate.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpDueDate.Location = new System.Drawing.Point(3, 378);
+            this.dtpDueDate.Name = "dtpDueDate";
+            this.dtpDueDate.Size = new System.Drawing.Size(145, 22);
+            this.dtpDueDate.TabIndex = 10;
+            // 
+            // btnAddWithDate
+            // 
+            this.btnAddWithDate.Location = new System.Drawing.Point(296, 377);
+            this.btnAddWithDate.Name = "btnAddWithDate";
+            this.btnAddWithDate.Size = new System.Drawing.Size(158, 23);
+            this.btnAddWithDate.TabIndex = 11;
+            this.btnAddWithDate.Text = "Add with Due Date";
+            this.btnAddWithDate.UseVisualStyleBackColor = true;
+            this.btnAddWithDate.Click += new System.EventHandler(this.btnAddWithDate_Click);
+            // 
+            // grpPriority
+            // 
+            this.grpPriority.Controls.Add(this.rdoLow);
+            this.grpPriority.Controls.Add(this.rdoMedium);
+            this.grpPriority.Controls.Add(this.rdoHigh);
+            this.grpPriority.Location = new System.Drawing.Point(235, 330);
+            this.grpPriority.Name = "grpPriority";
+            this.grpPriority.Size = new System.Drawing.Size(202, 42);
+            this.grpPriority.TabIndex = 12;
+            this.grpPriority.TabStop = false;
+            this.grpPriority.Text = "Priority";
+            // 
+            // rdoHigh
+            // 
+            this.rdoHigh.AutoSize = true;
+            this.rdoHigh.Location = new System.Drawing.Point(10, 20);
+            this.rdoHigh.Name = "rdoHigh";
+            this.rdoHigh.Size = new System.Drawing.Size(59, 20);
+            this.rdoHigh.TabIndex = 0;
+            this.rdoHigh.TabStop = true;
+            this.rdoHigh.Text = "High ";
+            this.rdoHigh.UseVisualStyleBackColor = true;
+            // 
+            // rdoMedium
+            // 
+            this.rdoMedium.AutoSize = true;
+            this.rdoMedium.Location = new System.Drawing.Point(70, 20);
+            this.rdoMedium.Name = "rdoMedium";
+            this.rdoMedium.Size = new System.Drawing.Size(79, 20);
+            this.rdoMedium.TabIndex = 1;
+            this.rdoMedium.TabStop = true;
+            this.rdoMedium.Text = "Medium ";
+            this.rdoMedium.UseVisualStyleBackColor = true;
+            // 
+            // rdoLow
+            // 
+            this.rdoLow.AutoSize = true;
+            this.rdoLow.Location = new System.Drawing.Point(140, 20);
+            this.rdoLow.Name = "rdoLow";
+            this.rdoLow.Size = new System.Drawing.Size(55, 20);
+            this.rdoLow.TabIndex = 2;
+            this.rdoLow.TabStop = true;
+            this.rdoLow.Text = "Low ";
+            this.rdoLow.UseVisualStyleBackColor = true;
+            // 
+            // frmToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(482, 403);
+            this.Controls.Add(this.grpPriority);
+            this.Controls.Add(this.btnAddWithDate);
+            this.Controls.Add(this.dtpDueDate);
+            this.Controls.Add(this.btnChangeTheme);
+            this.Controls.Add(this.chkDarkMode);
+            this.Controls.Add(this.btnMarkComplete);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblTotalTasks);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.lstTasks);
+            this.Controls.Add(this.txtTask);
+            this.Name = "frmToDoList";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "To-Do List Application";
+            this.grpPriority.ResumeLayout(false);
+            this.grpPriority.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTask;
+        private System.Windows.Forms.ListBox lstTasks;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblTotalTasks;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnMarkComplete;
+        private System.Windows.Forms.CheckBox chkDarkMode;
+        private System.Windows.Forms.Button btnChangeTheme;
+        private System.Windows.Forms.DateTimePicker dtpDueDate;
+        private System.Windows.Forms.Button btnAddWithDate;
+        private System.Windows.Forms.GroupBox grpPriority;
+        private System.Windows.Forms.RadioButton rdoMedium;
+        private System.Windows.Forms.RadioButton rdoHigh;
+        private System.Windows.Forms.RadioButton rdoLow;
+    }
+}
+
---
+++ ToDoListApp/ToDoListApp/Form1.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoListApp
+{   
+    public partial class frmToDoList : Form
+    {
+        ColorDialog colorDialog = new ColorDialog();
+        public frmToDoList()
+        {
+            InitializeComponent();
+            chkDarkMode.CheckedChanged += new EventHandler(chkDarkMode_CheckedChanged);
+            btnChangeTheme.Click += new EventHandler(btnChangeTheme_Click);
+            lstTasks.DrawItem += new DrawItemEventHandler(lstTasks_DrawItem);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                lstTasks.Items.Add(txtTask.Text);
+                txtTask.Clear();
+                UpdateTaskCount();
+                CheckTaskDeadlines();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a task!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                txtTask.Text = lstTasks.SelectedItem.ToString();
+                lstTasks.Items.RemoveAt(lstTasks.SelectedIndex);
+                UpdateTaskCount();
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to edit!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                lstTasks.Items.RemoveAt(lstTasks.SelectedIndex);
+                UpdateTaskCount();
+                CheckTaskDeadlines();
+            }
+            else
+            {
+                MessageBox.Show("Please select a task to delete!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void btnMarkComplete_Click(object sender, EventArgs e)
+        {
+            if (lstTasks.SelectedIndex != -1)
+            {
+                string selectedTask = lstTasks.SelectedItem.ToString();
+                if (!selectedTask.StartsWith("✔ "))
+                {
+                    lstTasks.Items[lstTasks.SelectedIndex] = "✔ " + selectedTask;
+                    CheckTaskDeadlines();
+                }
+                else
+                {
+                    MessageBox.Show("This task is already marked as completed!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }          
+            else
+            {
+                MessageBox.Show("Please select a task to mark as completed!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void UpdateTaskCount()
+        {
+            lblTotalTasks.Text = "Total Tasks: " + lstTasks.Items.Count;
+        }
+
+        private void chkDarkMode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkDarkMode.Checked)
+            {
+                this.BackColor = Color.FromArgb(45, 45, 48); // Koyu Gri
+                lblTitle.ForeColor = Color.White;
+                lblTotalTasks.ForeColor = Color.White;
+                txtTask.BackColor = Color.FromArgb(30, 30, 30);
+                txtTask.ForeColor = Color.White;
+                lstTasks.BackColor = Color.FromArgb(30, 30, 30);
+                lstTasks.ForeColor = Color.White;
+                btnAdd.BackColor = Color.Gray;
+                btnEdit.BackColor = Color.Gray;
+                btnDelete.BackColor = Color.Gray;
+                btnMarkComplete.BackColor = Color.Gray;
+            }
+            else
+            {
+                this.BackColor = Color.LightSteelBlue;
+                lblTitle.ForeColor = Color.Navy;
+                lblTotalTasks.ForeColor = Color.DarkBlue;
+                txtTask.BackColor = Color.White;
+                txtTask.ForeColor = Color.Black;
+                lstTasks.BackColor = Color.White;
+                lstTasks.ForeColor = Color.Black;
+                btnAdd.BackColor = Color.LightGreen;
+                btnEdit.BackColor = Color.LightBlue;
+                btnDelete.BackColor = Color.LightCoral;
+                btnMarkComplete.BackColor = Color.Orange;
+            }
+        }
+
+        private void btnChangeTheme_Click(object sender, EventArgs e)
+        {
+              
+                if (colorDialog.ShowDialog() == DialogResult.OK) // Kullanıcı "Tamam" dediğinde uygula
+                {
+                    Color SelectedColor = colorDialog.Color; // Seçilen rengi kaydet
+                    
+                    this.BackColor = colorDialog.Color;
+                    txtTask.BackColor = colorDialog.Color;
+                    lstTasks.BackColor= colorDialog.Color;
+                    grpPriority.BackColor = colorDialog.Color;
+
+                    btnAdd.BackColor = ControlPaint.Dark(colorDialog.Color);
+                    btnEdit.BackColor = ControlPaint.Dark(colorDialog.Color);
+                    btnDelete.BackColor = ControlPaint.Dark(colorDialog.Color);
+                    btnMarkComplete.BackColor = ControlPaint.Dark(colorDialog.Color);
+
+                }
+        }
+        
+
+        private void btnAddWithDate_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtTask.Text))
+            {
+                string taskText = txtTask.Text;
+                string dueDate = dtpDueDate.Value.ToShortDateString(); // Seçilen tarihi al
+                string priority = GetTaskPriority(); // Öncelik belirleme
+                string finalTask = $"{priority}{taskText} - Due: {dueDate}"; // Son hali
+               
+                lstTasks.Items.Add(finalTask);
+                txtTask.Clear();
+                UpdateTaskCount();
+                CheckTaskDeadlines(); // Tarih kontrolü yap               
+            }
+            else
+            {
+                MessageBox.Show("Please enter a task!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void CheckTaskDeadlines()
+        {            
+            for (int i = 0; i < lstTasks.Items.Count; i++)
+            {
+                string task = lstTasks.Items[i].ToString();
+                if (task.Contains("Due:"))
+                {
+                    string dateString = task.Substring(task.IndexOf("Due:") + 5);
+                    if (DateTime.TryParse(dateString, out DateTime dueDate))
+                    {
+                        TimeSpan difference = dueDate - DateTime.Today;
+
+                        if (difference.TotalDays < 0) // Tarih geçtiyse kırmızı
+                        {
+                            lstTasks.Items[i] =task.StartsWith("⛔" ) ? task : $"⛔{task}";
+                        }
+                        else if (difference.TotalDays <= 2) // 2 gün içinde yaklaşan görevleri sarı yap
+                        {
+                            lstTasks.Items[i] = task.StartsWith("⚠") ? task : $"⚠ {task}";
+                        }
+                    }
+                }
+            }
+            lstTasks.Refresh();
+        }
+        private void lstTasks_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            if (e.Index < 0) return;
+
+            e.DrawBackground();
+
+            string taskText = lstTasks.Items[e.Index].ToString();
+            Color textColor = Color.Black;
+
+            if (taskText.Contains("[High]")) textColor = Color.Red;
+            else if (taskText.Contains("[Medium]")) textColor = Color.Blue;
+            else if (taskText.Contains("[Low]")) textColor = Color.Green;
+            else if (taskText.StartsWith("⛔")) textColor = Color.DarkRed; // Geçmiş görevler
+            else if (taskText.StartsWith("⚠")) textColor = Color.Orange;  // Yaklaşan görevler
+
+            using (Brush brush = new SolidBrush(textColor))
+            {
+                e.Graphics.DrawString(taskText, e.Font, brush, e.Bounds);
+            }
+
+            e.DrawFocusRectangle();
+        }
+        private string GetTaskPriority()
+        {
+            if (rdoHigh.Checked) return "[High] ";
+            if (rdoMedium.Checked) return "[Medium] ";
+            if (rdoLow.Checked) return "[Low] ";
+            return "";
+        }
+    }
+}
+
+
+
+
+
+
+
